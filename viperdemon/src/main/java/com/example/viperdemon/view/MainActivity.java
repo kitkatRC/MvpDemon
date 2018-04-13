70b2006a479a3113c7db6e977b449305499b0a5d
kitkatRC <stephen.ruan@oyorooms.com>
2018-04-13 09:06:53 +0800

    test4111commit

--- viperdemon/src/main/java/com/example/viperdemon/view/MainActivity.java
+++ viperdemon/src/main/java/com/example/viperdemon/view/MainActivity.java
@@ -72,4 +72,7 @@ public class MainActivity extends AppCompatActivity implements BaseContacts.view
     //test------------------------------------------123
     //test----ce111
     //test----------ce222222222222
+
+
+    //test4----------------------111111
 }
